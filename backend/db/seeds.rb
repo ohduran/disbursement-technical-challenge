2cd2925bbdffdcd920f9ad425d0ba3216db0afe7
ohduran <alvaro.duranb@hotmail.com>
2023-01-22 12:58:02 +0100

    Preload data via seeds

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'json'
+
+merchants_data = JSON.parse(File.read('db/fixtures/merchants.json'))
+merchants_data['RECORDS'].each do |merchant_data|
+  Merchant.create!(merchant_data)
+end
+p "Created #{Merchant.count} merchants"
+
+shoppers_data = JSON.parse(File.read('db/fixtures/shoppers.json'))
+shoppers_data['RECORDS'].each do |shopper_data|
+  Shopper.create!(shopper_data)
+end
+p "Created #{Shopper.count} shoppers"
+
+orders_data = JSON.parse(File.read('db/fixtures/orders.json'))
+orders_data['RECORDS'].each do |order_data|
+  Order.create!(order_data)
+end
+p "Created #{Order.count} orders"
